7b80c408bccad255f49d5e8c5400e4df9f2a5a26
fariskakkodi <156721974+fariskakkodi@users.noreply.github.com>
2026-03-22 20:15:56 -0400

    Update DataModules.py

--- DataModules.py
+++ DataModules.py
@@ -28,7 +28,7 @@ class SequenceDataset(Dataset):
                     university = str(row["UNIV"]).strip() if "UNIV" in row and row["UNIV"] else "NA"
 
                     # include rubric as an additional feature
-                    line = CLS_TOKEN + student_answer + SEP_TOKEN + reference_answer
+                    line = CLS_TOKEN + question_text + SEP_TOKEN + student_answer + SEP_TOKEN + reference_answer + SEP_TOKEN + rubric
 
                     self.label_set.add(label)
                     self.data_dict.append({
