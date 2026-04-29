369bf6ea1bc790b19fd4f7467403c5ef79cb25b0
fariskakkodi <156721974+fariskakkodi@users.noreply.github.com>
2026-04-29 18:58:31 -0400

    Include rubric in data line construction
    Added rubric to the line construction for data processing.

--- DataModules.py
+++ DataModules.py
@@ -30,7 +30,9 @@ class SequenceDataset(Dataset):
 
                     # include rubric as an additional feature
                     #line = CLS_TOKEN + student_answer + SEP_TOKEN + reference_answer #+ SEP_TOKEN + rubric question_text + SEP_TOKEN
-                    line = CLS_TOKEN + question_text + SEP_TOKEN + student_answer + SEP_TOKEN + reference_answer
+                    #line = CLS_TOKEN + question_text + SEP_TOKEN + student_answer + SEP_TOKEN + reference_answer
+                    line = CLS_TOKEN + question_text + SEP_TOKEN + student_answer + SEP_TOKEN + reference_answer + SEP_TOKEN + rubric
+
 
                     self.label_set.add(label)
                     self.data_dict.append({
